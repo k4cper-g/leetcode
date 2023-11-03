c2ee550bcc1b8634e456d8d59656cc1e23a0d3c8
Kacper Gadomski <93160987+k4cper-g@users.noreply.github.com>
2023-11-03 22:00:46 +0100

    Create topK_frequent_elements.java

---
+++ topK_frequent_elements.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            if(!map.containsKey(number)) {
+                map.put(number, 1);
+            } else {
+                map.put(number, map.get(number) + 1);
+            }
+        }
+
+        int[] topFrequent = new int[k];
+
+        for(int j = 0; j < k; j++) {
+            int maxKey = Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+            topFrequent[j] = maxKey;
+            map.remove(maxKey);
+        }
+        return topFrequent;
+    }
+}
