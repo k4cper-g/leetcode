3ab0a46898cb8a9e34a7fd5231e0f043ae65e542
Kacper Gadomski <93160987+k4cper-g@users.noreply.github.com>
2023-11-03 16:42:16 +0100

    Create group_anagrams.java

---
+++ group_anagrams.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String word: strs) {
+            char[] letters = word.toCharArray();
+            Arrays.sort(letters);
+            String sortedWord = new String(letters);
+
+            if(!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            } 
+            
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
