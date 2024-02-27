ffbc310a79ad64c4182587c928657074e6d4d7ce
Kacper Gadomski <93160987+k4cper-g@users.noreply.github.com>
2024-02-27 16:04:52 +0100

    Create remove_duplicates_from_sorted_array_2.py

---
+++ remove_duplicates_from_sorted_array_2.py
@@ -0,0 +1,43 @@
+# Given an integer array nums sorted in non-decreasing order,
+# remove some duplicates in-place such that each unique element appears at most twice.
+# The relative order of the elements should be kept the same.
+#
+# Since it is impossible to change the length of the array in some languages,
+# you must instead have the result be placed in the first part of the array nums.
+# More formally, if there are k elements after removing the duplicates,
+# then the first k elements of nums should hold the final result.
+# It does not matter what you leave beyond the first k elements.
+#
+# Return k after placing the final result in the first k slots of nums.
+#
+# Do not allocate extra space for another array.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i = 1
+        tmp = nums[0]
+
+        if len(nums) <= 1:
+            return len(nums)
+
+        while i < len(nums):
+            if nums[i] == tmp and nums.count(nums[i]) > 2:
+                nums.remove(nums[i])
+                continue
+            elif nums[i] != tmp:
+                tmp = nums[i]
+
+            i += 1
+
+        print(nums)
+
+        return len(nums)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+sol = Solution()
+
+print(sol.removeDuplicates([1, 1, 1, 2, 2, 2, 3]))
